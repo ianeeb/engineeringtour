9f5166b225f54cc1c4b3c95363bba665e4a4f34f
ianeeb <95736356+ianeeb@users.noreply.github.com>
2021-12-10 11:11:28 +0800

    a
    First upload of all files

---
+++ data.js
@@ -0,0 +1,111 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-engineering-auditorium-atrium",
+      "name": "Engineering Auditorium Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.2458965737313346,
+        "pitch": -0.13314230810665073,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.291777095470792,
+        "pitch": -0.19400736324112344,
+        "fov": 1.2933824216075565
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9773455311213333,
+          "pitch": -0.26800135309476936,
+          "rotation": 0,
+          "target": "0-along-engineering-drive-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "FoE",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
